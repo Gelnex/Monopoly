d02307f5889ece92c1eeadc426cc05f4d6651fdb
Engels Felix <fxmega.endercraft1200@gmail.com>
2024-03-08 22:44:53 +0100

    ajout des case polices et prof par diogo

---
+++ Case_Police.py
@@ -0,0 +1,25 @@
+"""
+Arriaga Diogo 
+08-03-2024
+Case police qui donne des malus 
+"""
+### Imports nécessaires ###
+from Case import *
+
+### Définition de la classe ###
+class Case_Police (Case):
+    def __init__(self, nom: str, coordonee: int) -> None:
+        super().__init__(nom, coordonee)
+
+    ### Action que la case peut faire ###
+    def Action01 (self, joueur):
+        joueur.bouger(-3)
+        print (joueur.position)
+
+    def Action02 (self, joueur):
+        joueur.donner_argent(-100)
+        print (joueur.argent)
+
+
+### Tests ###
+case = Case_Police("Police", 3)
